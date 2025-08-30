f77756504855f656891b9dd10a4687cf32b66458
Shukla9289 <Satyam.personal123@gmail.com>
2025-08-30 09:33:19 +0000

    data types and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,9 @@
+let Name="Satyam Shukla";
+let age=22;
+let islogged_in=true;
+
+console.log(typeof(Name));
+console.log(typeof(age));
+console.log(typeof(islogged_in));
+console.log(typeof(null));  //null is an object
+console.log(typeof(undefined));
---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountid=1445;
+let accountmail="satyam@gmail.com"
+var accountpassword="1223"
+
+console.log(accountid);
+accountcity="Delhi";
+
+accountcity="Bengaluru"
+console.table([accountid,accountmail,accountpassword,accountcity])
+//Var ko mat use karo
