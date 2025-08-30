2613021509c4836c6c97ccfe926aaf3ca1e91177
Shukla9289 <Satyam.personal123@gmail.com>
2025-08-30 10:14:36 +0000

    conversion of datatypes

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,15 @@
+let score =33;
+console.log(typeof(score));
+
+ score="33";
+ console.log(typeof(score));
+
+// Conversion from string to number
+
+let valueinnumber=Number(score);
+console.log(typeof(valueinnumber));
+
+//Number to boolean
+let islogged_in=1
+let booleanisloggedin=Boolean(islogged_in);
+console.log(booleanisloggedin);
