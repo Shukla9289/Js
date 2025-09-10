65eee02973dbeb888dba1fdb34127ab860b5d981
Shukla9289 <Satyam.personal123@gmail.com>
2025-09-10 19:50:24 +0000

    Taking input from user

---
+++ 01_basics/practice.js
@@ -0,0 +1,6 @@
+//Taking input from user by prompt function
+const prompt = require("prompt-sync")();
+const userinput=prompt("Enter your name ","ABC" );
+const userage=prompt("Enter your age ","null");
+console.log("User name is : "+ userinput);
+console.log("User age is : "+ userage);
