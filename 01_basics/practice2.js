6db125c6ddbcd3a8373dc5b83621d5389aaedc53
Shukla9289 <Satyam.personal123@gmail.com>
2025-09-10 19:53:30 +0000

    For checking the number is odd or even

---
+++ 01_basics/practice2.js
@@ -0,0 +1,9 @@
+// For checking number is odd or even
+const prompt = require("prompt-sync")();
+const number=prompt("Enter the number :");
+if(number%2==0){
+    console.log("Number is Even");
+}
+else{
+    console.log("Number is Odd");
+}
