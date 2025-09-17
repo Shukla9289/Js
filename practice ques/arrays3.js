cfd088da6cf7dcc8191c01291d196b4ed1f5d59a
Shukla9289 <Satyam.personal123@gmail.com>
2025-09-17 18:43:28 +0000

    Arrays practice ques

---
+++ practice ques/arrays3.js
@@ -0,0 +1,8 @@
+let companies=["Bloomberg","Microsoft","uber","Google","IBM","Netflix"];
+companies.shift();
+console.log(companies);
+companies.splice(2,1,"Ola");
+console.log(companies);
+companies.push("Amazon");
+console.log(companies);
+//Practice set completed
